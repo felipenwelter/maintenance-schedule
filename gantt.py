09a695097b9ae8a0a14dcecc02c4b27355eeae00
felipenwelter <felipenwelter@gmail.com>
2020-10-20 21:25:39 -0300

    clean, comment and format

--- gantt.py
+++ gantt.py
@@ -3,24 +3,32 @@
 import plotly.figure_factory as ff
 import jsonManipulate as jm
 import pandas as pd
-
 import datetime
 
-
-
-def myFunc(e):
+#---------------------------------------------------------------
+# Função: sortingRule
+# Parâmetros: e - the item of the list
+# Retorno: defines the sorting rule of list_os
+#---------------------------------------------------------------
+def sortingRule(e):
   return e['start']
 
+#---------------------------------------------------------------
+# Função: getIdle
+# Parâmetros: list_so - list of service_orders
+# Retorno: using list_os, fill all the blanks between tasks
+#          with what is called idle schedules (list_idle)
+#---------------------------------------------------------------
 def getIdle(list_so):
     index = 0
     list_idle = []
     while index < len(list_so):
         if index == 0:
             list_idle.append({ 'employee': list_so[index]['employee'],
-                            'start': start_date, 'end': list_so[index]['start'] })
+                               'start': start_date, 'end': list_so[index]['start'] })
         else:
             list_idle.append({ 'employee': list_so[index]['employee'],
-                            'start': list_so[index-1]['end'], 'end': list_so[index]['start'] })
+                               'start': list_so[index-1]['end'], 'end': list_so[index]['start'] })
         index += 1
 
     # fill the last time with idle period, to show gantt
@@ -31,6 +39,13 @@ def getIdle(list_so):
     return list_idle
 
 
+#---------------------------------------------------------------
+# Função: getUnavailable
+# Parâmetros: list_idle - list of idle times
+# Retorno: using list_idle, uses the employee shift to divide
+#          what is really idle (based on work_shift) and what
+#          is unavailability, the time off the schedule
+#---------------------------------------------------------------
 def getUnavailable(list_idle):
 
     # check if there are itens with different start/end day, then separate day by day
@@ -54,15 +69,13 @@ def getUnavailable(list_idle):
         index += 1 
 
     # order by start date
-    list_idle.sort(key=myFunc)
+    list_idle.sort(key=sortingRule)
 
     index = 0
     while index < len(list_idle):
         weekday = (datetime.datetime.strptime(list_idle[index]['start'], '%Y-%m-%d %H:%M')).weekday()
         shift = work_shift[0]['shift'][weekday]
         
-        #index2 = 0
-        #while index2 < len(shift):
         start = list_idle[index]['start'][11:]
         end = list_idle[index]['end'][11:]
 
@@ -89,7 +102,6 @@ def getUnavailable(list_idle):
                 list_idle[index]['start'] = list_idle[index]['start'][:11] + shift[0]
                 list_idle[index]['end'] = list_idle[index]['end'][:11] + shift[1]
 
-                #index2 += 1
         else: #if len(shift) == 0
             list_unavailable.append({ 'employee': list_idle[index]['employee'],
                                         'start': list_idle[index]['start'], 
@@ -114,15 +126,10 @@ def getUnavailable(list_idle):
 
 
 
-
-
 wl = jm.loadJSON()
-
 df = []
 
 #### TODO
-# check if it is possible to show the time as hint
-# use colors to identify: green - allocated; gray - unavaiable, red - idle
 # calcular qual a ociosidade geral e por funcionario no comeco e depois no final, pra ver se aumenta ou diminui
 
 start_date = '2020-10-01 00:00'
@@ -131,18 +138,18 @@ list_so = []
 list_idle = []
 list_unavailable = []
 work_shift = [ { 'employee': 'Luiza',
-                 'shift': [['08:00','18:00'], ['08:00','18:00'], ['08:00','18:00'], ['09:00','14:00'], ['08:00','18:00'],'',''] }  ]
+                'shift': [['08:00','18:00'], ['08:00','18:00'], ['08:00','18:00'], ['09:00','14:00'], ['08:00','18:00'],'',''] }  ]
 
 for so in wl['workload']:
     new = dict(Task=so['employee'], Start=so['start'],
-            Finish=so['end'], Resource='busy') #Resource=so['number'])
+               Finish=so['end'], Resource='busy')
     df.append(new)
     
     if (so['employee'] == 'Nelson'):
         list_so.append({ 'employee': so['employee'], 'start': so['start'], 'end': so['end'] })
 
 # order the list by start date
-list_so.sort(key=myFunc)
+list_so.sort(key=sortingRule)
 
 #after getting all the service orders, update the last date to gantt
 end_date = list_so[-1]['end'][:11]+"23:59"
@@ -157,33 +164,25 @@ list_idle = getIdle(list_so)
 # extract the unavaiable periods into the idle periods identified previously
 list_unavailable = getUnavailable(list_idle)
 
-        
-    
-
-    
-
 
 # add the idle into the gantt chart
 for so in list_idle:
     new = dict(Task=so['employee'], Start=so['start'],
-            Finish=so['end'], Resource='idle') #Resource=so['number'])
+               Finish=so['end'], Resource='idle')
     df.append(new)
 
 # add the unavailable into the gantt chart
 for so in list_unavailable:
     new = dict(Task=so['employee'], Start=so['start'],
-            Finish=so['end'], Resource='unavailable') #Resource=so['number'])
+               Finish=so['end'], Resource='unavailable')
     df.append(new)
 
-#colors = {'000881': 'rgb(220, 0, 0)',
-#          '009372': (1, 0.9, 0.16),
-#          '003711': (0.1, 0.1, 0.9),
-#          '034830': 'rgb(100, 255, 100)'}
-
 colors = {'busy': 'rgb(30,144,255)',
-          'idle': 'rgb(230,90,90)',
-          'unavailable': 'rgb(205,205,205)'}
+        'idle': 'rgb(230,90,90)',
+        'unavailable': 'rgb(205,205,205)'}
 
-fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=True,
-                      group_tasks=True)
+fig = ff.create_gantt(df, colors=colors, index_col='Resource',
+                    show_colorbar=True, group_tasks=True)
 fig.show()
+
+
